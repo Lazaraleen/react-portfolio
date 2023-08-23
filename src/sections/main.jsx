6b051e7935c88adeeff50c4962b2f92369f9f711
Myriam JAYLE <myriamjayle@gmail.com>
2023-08-23 16:56:48 +0200

    media queries Home

--- src/sections/main.jsx
+++ src/sections/main.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import Home from './home';
-// import NavBar from "./navbar";
-// import Skills from "./skills";
-// import Education from "./education";
-// import Experience from "./experience";
-// import Works from "./works";
-// import Contact from "./contact";
+import NavBar from "./navbar";
+import Skills from "./skills";
+import Education from "./education";
+import Experience from "./experience";
+import Works from "./works";
+import Contact from "./contact";
 
 function Main() {
   return (
     <main>
-      {/* <div className="barNav">
+      <div className="barNav">
         <NavBar />
-      </div> */}
+      </div>
       <div className="page">
         <Home />
-        {/* <Skills />
+        <Skills />
         <Education />
         <Experience />
         <Works />
-        <Contact /> */}
+        <Contact />
       </div>
     </main>
 
